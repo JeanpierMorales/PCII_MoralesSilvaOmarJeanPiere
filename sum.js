b862da2b4ef3cb26e651c65f50be4cb52a860727
Jeanpier Morales <omar.morales@usil.pe>
2025-11-18 21:34:46 -0500

    Ejercicio 3

--- sum.js
+++ sum.js
@@ -1,4 +1,6 @@
+// Función que suma dos números
 function sum(a, b) {
   return a + b;
 }
+// Exportar la función para usarla en otros archivos
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -1,12 +1,12 @@
 const sum = require("./sum");
 
 describe("Pruebas para la función sum", () => {
-  // Validar que sum(4, 6) devuelve 10.
+  // Validar que la suma de 4 y 6 nos devuelva 10
   test("Debe sumar 4 y 6 correctamente para obtener 10", () => {
     expect(sum(4, 6)).toBe(10);
   });
 
-  // Validar que sum(-3, 8) devuelve 5.
+  // Valida que la suma de -3 y 8 nos devuelva 5
   test("Debe sumar -3 y 8 correctamente para obtener 5", () => {
     expect(sum(-3, 8)).toBe(5);
   });
